9dcd3377a2012bc13bcf38f400359b41e7920eb8
Olin Kirkland <me@olinkirk.land>
2023-08-10 22:52:40 +0200

    Add mixpanel

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Card from './components/card.vue';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import mixpanel from 'mixpanel-browser';
 
 // Create the app
 const app = createApp(App);
@@ -14,3 +15,7 @@ app.use(router);
 
 // Mount the app
 app.mount('#app');
+
+// Initialize Mixpanel
+mixpanel.init('c270f1816c518d251d2b2414daf17d84');
+mixpanel.track('initial-load');
