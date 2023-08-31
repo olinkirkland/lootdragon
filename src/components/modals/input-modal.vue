06e9dabdd7c813ae230600d122b429aec649efd2
Olin Kirkland <olin.kirkland@beg.de>
2023-08-31 13:19:17 +0200

    Commit input-modal

---
+++ src/components/modals/input-modal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="modal" :class="{ busy: isBusy }">
+    <header>
+      <h2>{{ props.title }}</h2>
+      <button class="icon" @click="ModalController.close">
+        <i class="fas fa-times"></i>
+      </button>
+    </header>
+    <div class="modal__content">
+      <section>
+        <p>{{ props.text }}</p>
+        <input type="text" v-model="input" :placeholder="props.placeholder" />
+      </section>
+      <section>
+        <div class="flex">
+          <button @click="ModalController.close" v-if="props.cancelText">
+            {{ props.cancelText }}
+          </button>
+          <button
+            class="primary"
+            @click="busyAndCallback"
+            v-if="props.confirmText"
+          >
+            <span>{{ props.confirmText }}</span>
+          </button>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ModalController } from '@/controllers/modal-controller';
+import { PropType, ref } from 'vue';
+
+const isBusy = ref(false);
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: 'Input something'
+  },
+  text: {
+    type: String,
+    required: false,
+    default: 'Enter the text below:'
+  },
+  confirmText: {
+    type: String,
+    required: false,
+    default: 'Confirm'
+  },
+  confirmCallback: {
+    type: Function as PropType<(response: string) => void>,
+    required: false,
+    default: () => ModalController.close()
+  },
+  cancelText: {
+    type: String,
+    required: false,
+    default: 'Cancel'
+  },
+  cancelCallback: {
+    type: Function as PropType<() => void>,
+    required: false,
+    default: () => ModalController.close()
+  },
+  placeholder: {
+    type: String,
+    required: false,
+    default: 'Type here...'
+  }
+});
+
+const input = ref('');
+
+const busyAndCallback = async () => {
+  isBusy.value = true;
+  await props.confirmCallback(input.value);
+  isBusy.value = false;
+};
+</script>
+
+<style scoped lang="scss"></style>
