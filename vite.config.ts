fc5f2c180b072002c613bad7498af3e53265b6ba
Olin Kirkland <me@olinkirk.land>
2024-08-11 19:22:44 +0200

    Fix subdomain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lootdragon',
+  base: 'lootdragon',
   plugins: [vue()],
   server: { host: false }, // For external IP access
   resolve: {
